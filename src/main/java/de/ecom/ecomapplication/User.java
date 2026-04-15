11721ec7915017a4d728612da25c80e992f4c0fa
Karim <karimentwickler@gmail.com>
2026-04-15 22:55:26 +0200

    Implementig User Entity and JPA mapping

--- src/main/java/de/ecom/ecomapplication/User.java
+++ src/main/java/de/ecom/ecomapplication/User.java
@@ -1,10 +1,23 @@
 package de.ecom.ecomapplication;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Value;
 
 @Data
+@Entity(name= "users")
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String firstName;
     private String lastName;
 }
