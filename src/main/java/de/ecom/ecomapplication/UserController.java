d85750890ff214882bc8e199c3eba3366e296579
Karim <karimentwickler@gmail.com>
2026-04-15 21:46:33 +0200

    Handle user endpoints  with global Requestmapping

--- src/main/java/de/ecom/ecomapplication/UserController.java
+++ src/main/java/de/ecom/ecomapplication/UserController.java
@@ -8,15 +8,16 @@ import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/api/users")
 public class UserController {
     private final UserService userService;
 
-    @GetMapping("/api/users")
+    @GetMapping
     public ResponseEntity<List<User>> getAllUsers() {
         return ResponseEntity.ok(userService.fetchAllUsers());
     }
 
-    @GetMapping("/api/users/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<User> getUser(@PathVariable Long id) {
 
         return userService.fetchAUser(id)
@@ -24,18 +25,18 @@ public class UserController {
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @PostMapping("/api/users")
+    @PostMapping
     public ResponseEntity<String> createUser(@RequestBody User user) {
         userService.addUser(user);
         return ResponseEntity.ok("User Added Successfully");
 
     }
 
-    @PutMapping("/api/users/{id}")
-    public ResponseEntity<User> getUser(@PathVariable Long id, @RequestBody User updatedUser) {
+    @PutMapping("/{id}")
+    public ResponseEntity<String> getUser(@PathVariable Long id, @RequestBody User updatedUser) {
         boolean updated = userService.updateUser(id, updatedUser);
         if (updated)
-            return ResponseEntity.ok(updatedUser);
+            return ResponseEntity.ok("User updated successfully");
         return ResponseEntity.notFound().build();
     }
 }
