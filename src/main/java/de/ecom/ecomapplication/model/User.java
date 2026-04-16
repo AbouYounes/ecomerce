4118483447993860b85f07358446e08caa6487c7
Karim <karimentwickler@gmail.com>
2026-04-16 22:07:06 +0200

    Define adress Entity and Relationship with user

--- src/main/java/de/ecom/ecomapplication/model/User.java
+++ src/main/java/de/ecom/ecomapplication/model/User.java
@@ -1,16 +1,15 @@
 package de.ecom.ecomapplication.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 @Data
 @Entity(name= "users")
-@AllArgsConstructor
 @NoArgsConstructor
 public class User {
     @Id
@@ -20,7 +19,16 @@ public class User {
     private String firstName;
     private String lastName;
     private String email;
-    private String password;
-    private String phoneNumber;
+    private String phone;
     private UserRole role= UserRole.CUSTOMER;
+
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "")
+    private Address address;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 }
